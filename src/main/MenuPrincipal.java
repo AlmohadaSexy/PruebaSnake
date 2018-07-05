82f5696accdd7e25acd6b9a0f107f958dc8775f5
Exeltor <zhelezovalexey@gmail.com>
2018-07-05 18:22:15 +0200

    Reformateado el menu principal

--- src/main/MenuPrincipal.java
+++ src/main/MenuPrincipal.java
@@ -20,21 +20,7 @@ public class MenuPrincipal{
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    MenuPrincipal window = new MenuPrincipal();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    public void open() {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    MenuPrincipal window = new MenuPrincipal();
-                    window.frame.setVisible(true);
+                    new MenuPrincipal();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -44,6 +30,7 @@ public class MenuPrincipal{
 
     public MenuPrincipal() {
         initialize();
+        frame.setVisible(true);
     }
 
     private void initialize() {
@@ -52,6 +39,7 @@ public class MenuPrincipal{
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double anchuraPantalla = (screenSize.getWidth())/3;
         double alturaPantalla = (screenSize.getHeight()/2);
+        frame.setUndecorated(true);
 
         frame.setSize((int) anchuraPantalla, (int) alturaPantalla);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,9 +50,8 @@ public class MenuPrincipal{
         panel.setLayout(new GridLayout(3, 0));
         frame.setContentPane(panel);
 
-        JButton btn1 = new JButton("Sistemas de Ecuaciones");
+        JButton btn1 = new JButton("Jugar");
         btn1.setFont(new Font("Tahoma", Font.PLAIN, 26));
-        btn1.setToolTipText("Resuelve un sistema de 3 ecuaciones con 3 incognitas.");
         btn1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
@@ -73,9 +60,8 @@ public class MenuPrincipal{
         });
         panel.add(btn1);
 
-        JButton btn2 = new JButton("Ecuaciones Conicas");
+        JButton btn2 = new JButton("Elegir dificultad");
         btn2.setFont(new Font("Tahoma", Font.PLAIN, 26));
-        btn2.setToolTipText("Resolucion y representacion grafica de ecuaciones conicas.");
         btn2.addActionListener(new ActionListener() {
             @SuppressWarnings("static-access")
             public void actionPerformed(ActionEvent e) {
@@ -85,13 +71,11 @@ public class MenuPrincipal{
         });
         panel.add(btn2);
 
-        JButton btn3 = new JButton("Crecimiento de Poblaciones");
+        JButton btn3 = new JButton("Salir");
         btn3.setFont(new Font("Tahoma", Font.PLAIN, 26));
-        btn3.setToolTipText("Calcular el crecimiento de una poblacion en x años.");
         btn3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-        //        new VentanaPoblacion().open();
+                System.exit(1);
             }
         });
         panel.add(btn3);
